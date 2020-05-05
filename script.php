d41259fcbe5176adeb532f0447c7561084af83af
Kellis <vikiclashonyt@gmail.com>
2020-05-05 03:20:03 +0200

    Update script.php

--- script.php
+++ script.php
@@ -2,5 +2,5 @@
 $bdd = new PDO('mysql:host=localhost;dbname=visits_ip', 'root', '');
 
 $jsonfileinsert = file_get_contents('http://ip-api.com/json/'.$_SERVER['REMOTE_ADDR']);
-$insertmbr3 = $bdd->prepare('INSERT INTO ip_list(ip, navigateur, date, json) VALUES(?, ?, UNIX_TIMESTAMP(), ?)');
-$insertmbr3->execute(array($_SERVER['REMOTE_ADDR'], $_SERVER["HTTP_USER_AGENT"], $jsonfileinsert));
+$insertmbr3 = $bdd->prepare('INSERT INTO ip_list(ip, navigateur, date, json, page) VALUES(?, ?, UNIX_TIMESTAMP(), ?, ?)');
+$insertmbr3->execute(array($_SERVER['REMOTE_ADDR'], $_SERVER["HTTP_USER_AGENT"], $jsonfileinsert, $_SERVER['SCRIPT_NAME']));
