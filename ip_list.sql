9b6ffadb9e472338d78557d269950ba60f7f51fb
Kellis <vikiclashonyt@gmail.com>
2020-05-26 22:20:55 +0200

    With the last change

--- ip_list.sql
+++ ip_list.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb4
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Client :  db5000217374.hosting-data.io
--- Généré le :  Dim 12 Janvier 2020 à 12:55
+-- Hôte : db5000217374.hosting-data.io
+-- Généré le : mar. 26 mai 2020 à 20:18
 -- Version du serveur :  5.7.28-log
--- Version de PHP :  7.0.33-0+deb9u6
+-- Version de PHP : 7.0.33-0+deb9u7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `dbs212220`
+-- Base de données : `dbs212220`
 --
 
 -- --------------------------------------------------------
@@ -31,11 +33,16 @@ CREATE TABLE `ip_list` (
   `ip` varchar(25) NOT NULL,
   `navigateur` varchar(255) NOT NULL,
   `date` int(15) NOT NULL,
-  `json` varchar(1000) NOT NULL
+  `json` varchar(1000) NOT NULL,
+  `page` varchar(255) NOT NULL,
+  `orientation` varchar(255) NOT NULL,
+  `screen` varchar(255) NOT NULL,
+  `viewport` varchar(255) NOT NULL,
+  `diagonal` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Index pour les tables exportées
+-- Index pour les tables déchargées
 --
 
 --
@@ -45,7 +52,7 @@ ALTER TABLE `ip_list`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT pour les tables exportées
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
@@ -53,6 +60,8 @@ ALTER TABLE `ip_list`
 --
 ALTER TABLE `ip_list`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
